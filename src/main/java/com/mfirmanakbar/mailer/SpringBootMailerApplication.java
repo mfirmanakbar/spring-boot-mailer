14a8af2113852e9b42f4c1be75fdf85a5052a112
mfirmanakbar <em.firman.akbar@gmail.com>
2019-06-06 08:44:01 +0700

    tested-ok
    succsesfully gmail sent

--- src/main/java/com/mfirmanakbar/mailer/SpringBootMailerApplication.java
+++ src/main/java/com/mfirmanakbar/mailer/SpringBootMailerApplication.java
@@ -1,13 +1,71 @@
 package com.mfirmanakbar.mailer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 
 @SpringBootApplication
-public class SpringBootMailerApplication {
+public class SpringBootMailerApplication implements CommandLineRunner {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMailerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Sending Email...");
+		
+		sendEmail();
+		// sendEmailWithAttachment();
+
+		System.out.println("Done");
+	}
+
+	void sendEmail() {
+
+		SimpleMailMessage msg = new SimpleMailMessage();
+		//msg.setTo("m.firman.aa@gmail.com", "2@yahoo.com");
+		msg.setTo("em.firman.akbar@gmail.com");
+
+		msg.setSubject("Testing from Spring Boot");
+		msg.setText("Hello World \n Spring Boot Email");
+
+		javaMailSender.send(msg);
+
+	}
+
+	void sendEmailWithAttachment() throws MessagingException, IOException {
+
+		MimeMessage msg = javaMailSender.createMimeMessage();
+
+		// true = multipart message
+		MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+		helper.setTo("1@gmail.com");
+
+		helper.setSubject("Testing from Spring Boot");
+
+		// default = text/plain
+		// helper.setText("Check attachment for image!");
+
+		// true = text/html
+		helper.setText("<h1>Check attachment for image!</h1>", true);
+
+		helper.addAttachment("my_photo.png", new ClassPathResource("android.png"));
+
+		javaMailSender.send(msg);
+
 	}
 
 }
